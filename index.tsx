be4513ea1ebdc83a514eba960158bba48c7a1581
Lalit <lalitvijay9480@gmail.com>
2022-01-18 14:40:40 +0530

    fix: return router instance

--- index.tsx
+++ index.tsx
@@ -1,15 +1,12 @@
-import React from 'react'
-import { useRouter } from 'next/router'
-
-export const Thing = () => {
-  return <div>Use Link Prefetch</div>
-}
+import React from 'react';
+import { useRouter } from 'next/router';
 
 export const usePrefetch = (links: string[]) => {
-  const router = useRouter()
+  const router = useRouter();
   React.useEffect(() => {
     links.forEach(link => {
-      router.prefetch(link)
-    })
-  }, [])
-}
+      router.prefetch(link);
+    });
+  }, []);
+  return router;
+};
